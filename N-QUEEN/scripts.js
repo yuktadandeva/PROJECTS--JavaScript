08f4609d26526a4f8fe2bfcbf7f438a298dcb996
yuktadandeva <153160647+yuktadandeva@users.noreply.github.com>
2024-07-07 00:38:18 +0530

    Add files via upload

---
+++ N-QUEEN/scripts.js
@@ -0,0 +1,14 @@
+// function isSafe(board, row, col, n)
+
+// // check column
+// for(let i=0 ; i<row ;i++){
+//     if(board[i][col])
+//         return false
+// }
+
+// // check upper-right
+// for(let i=row, j= col; i>=0 && j<n; i--,j++){
+
+// }
+
+// for()
